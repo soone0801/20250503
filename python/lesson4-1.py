669eaed8c8c99dcf517b0358f56ec3859e67d5cd
廖啓盛 <j159753852749632@gmail.com>
2025-05-24 11:15:21 +0800

    使用 Colab 建立

--- python/lesson4-1.py
+++ python/lesson4-1.py
@@ -36,30 +36,46 @@
         "    else:\n",
         "        return \"媽的 肥仔\"\n",
         "\n",
-        "height:int = int(input(\"請輸入身高(CM):\"))\n",
-        "weight:int = int(input(\"請輸入體重(KG):\"))\n",
+        "def main():\n",
+        "    height:int = int(input(\"請輸入身高(CM):\"))\n",
+        "    weight:int = int(input(\"請輸入體重(KG):\"))\n",
         "\n",
-        "bmi = caculate_bmi(height,weight)\n",
-        "print(f\"BMI = {bmi}\")\n",
-        "print(get_state(bmi))"
+        "    bmi = caculate_bmi(height,weight)\n",
+        "    print(f\"BMI = {bmi}\")\n",
+        "    print(get_state(bmi))\n",
+        "if __name__ == \"__main__\":\n",
+        "    main()"
       ],
       "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "UBDuZKPLA668",
-        "outputId": "5fe30752-933c-45cc-ba5d-1c6d3b2c0b9e"
+        "id": "UBDuZKPLA668"
       },
-      "execution_count": 27,
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "vars_"
+      ],
+      "metadata": {
+        "id": "Dbvjos3AQVDf",
+        "outputId": "e7c8d631-3a6f-40f4-e797-7755e2570efd",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 147
+        }
+      },
+      "execution_count": 28,
       "outputs": [
         {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "請輸入身高(CM):187\n",
-            "請輸入體重(KG):74\n",
-            "BMI = 21.161600274528865\n",
-            "可以 正常\n"
+          "output_type": "error",
+          "ename": "NameError",
+          "evalue": "name 'vars_' is not defined",
+          "traceback": [
+            "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
+            "\u001b[0;31mNameError\u001b[0m                                 Traceback (most recent call last)",
+            "\u001b[0;32m<ipython-input-28-99d80ebf9bf4>\u001b[0m in \u001b[0;36m<cell line: 0>\u001b[0;34m()\u001b[0m\n\u001b[0;32m----> 1\u001b[0;31m \u001b[0mvars_\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m",
+            "\u001b[0;31mNameError\u001b[0m: name 'vars_' is not defined"
           ]
         }
       ]
